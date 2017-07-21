5acc0549495475cf070aa92431be4a5b72c4818f
Jianyu Zhou <zjybox@126.com>
2017-07-21 12:24:08 +0800

    finish filt

--- 职业举报人/script/ProInformant/main.py
+++ 职业举报人/script/ProInformant/main.py
@@ -7,15 +7,40 @@ import time
 def extract(data,extractNum):
     return data.iloc[:extractNum]
 
+def inValid(x):
+    if isinstance(x,str):
+        if x.startswith('无效举报'):
+            return False
+    return True
+
+def isPro(x):
+    if isinstance(x,str):
+        if x.startswith('本人'):
+            return True
 if __name__ == '__main__':
     startTime = time.time()
-    rawDataFileName = os.path.join('..','..','data','smallRawData.csv')
+    rawDataFileName = os.path.join('..','..','..','data','professional informant','20170501.csv')
+    colFileName = os.path.join('..','..','..','data','professional informant','20170501-职业举报人.csv')
+    colData = pd.read_csv(colFileName)
+    colNames = colData.columns
+    print(colNames)
     # print(rawDataFileName)
     rawData = pd.read_csv(rawDataFileName)
     # smallRawData = extract(rawData,3000)
     # smallRawData.to_csv(os.path.join('..','..','data','smallRawData.csv'), index=False)
     # print(rawData.shape)
-    columns = rawData.columns
-    print(columns[0])
+
+    filtData = rawData[(rawData['RECORD_IS_INVALID']==0)&\
+                       (rawData['RECORD_TYPE']!=3)&\
+                       (rawData['HANDLE_STATE']!=3)&\
+                       (rawData['REPLY_CONTENTS'].map(inValid))&\
+                        rawData['HANDLE_RESULT'].map(inValid)&\
+                        rawData['RECORD_HANDLE_DEVISECONTENTS'].map(inValid)]
+
+    filtData.RECORD_CONTENTS
+    print(filtData.shape)
+    outputFileName = os.path.join('..','..','..','data','professional informant','out.csv')
+    filtData.to_csv('tmp2.csv',index=False)
+
     endTime = time.time()
     print('finished,cost {} seconds'.format(endTime-startTime))
